61a9ba2dde9d1ba33f5264c26254552bf39c9bfa
JamizM <pedringuimaraes09@gmail.com>
2024-03-16 09:59:07 -0300

    tela inicial do app iniciada

--- SaoCamiloGestaoSaude/AppSaoCamilo/App.js
+++ SaoCamiloGestaoSaude/AppSaoCamilo/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./src/assets/logo_sao_camilo.png")} style={styles.logo_sao_camilo}/>
+      <Text styles={styles.tituloGestaoSaude}>YE-Gestão E Saúde</Text>
+
+      <TouchableOpacity style={styles.btnProximo}>
+        <Text style={styles.txtBtnProximo}>Proximo</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.txtConsulta}>Consulte seus exames médicos</Text>
+
     </View>
   );
 }
@@ -15,6 +22,34 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  logo_sao_camilo: {
+    marginTop: 60,
+    marginBottom: 60,
+    width: 200,
+    height: 200,
+  },
+  tituloGestaoSaude:{ //nao ta funcionando...
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  txtConsulta:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  btnProximo:{
+    backgroundColor: '#991A1A',
+    padding: 10,
+    borderRadius: 25,
+    marginTop: 20,
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+  },
+  txtBtnProximo:{
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
 });
